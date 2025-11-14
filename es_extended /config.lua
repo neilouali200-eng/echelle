700c8fcdad4e0c34b212cb4f19af087ed0c16ea3
neilouali200-eng <neil.ouali200@gmail.com>
2025-11-14 14:46:12 +0100

    Create config.lua

---
+++ es_extended /config.lua
@@ -0,0 +1,12 @@
+Config                          = {}
+Config.MaxPlayers               = 32
+Config.Accounts                 = { 'bank', 'black_money' }
+--Config.AccountLabels            = { bank = 'Banque', black_money = 'Argent Sale' } -- French
+Config.AccountLabels            = { bank = 'Bank', black_money = 'Dirty Money' } -- English
+Config.PaycheckInterval         = 10 * 60000
+Config.ShowDotAbovePlayer       = false
+Config.DisableWantedLevel       = false
+Config.RemoveInventoryItemDelay = 5 * 60000
+Config.EnableWeaponPickup       = true
+Config.Locale                   = 'en'
+Config.EnableDebug              = true
